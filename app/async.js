5dfbc8456772faa676759f8234de199dc301ca8f
Rupali Soni <rupali.soni19@gmail.com>
2016-07-28 19:34:33 -0400

    Js assessment

--- app/async.js
+++ app/async.js
@@ -2,10 +2,26 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
+	  var dfrd = $.Deferred();
+	// simulate async call w/ setTimeout
+	    setTimeout(function(){
+	      dfrd.resolve(value);
+	    }, 50);
+	return dfrd.promise();
 
   },
 
   manipulateRemoteData: function(url) {
-
+	  var dfrd = $.Deferred();
+	  $.ajax({
+		  url: url
+		}).done(function(data ) {
+			var names = data.people.map(function(item) {
+			    return item['name'];
+			});
+			dfrd.resolve(names.sort());
+			console.log(names);
+		});
+	  return dfrd.promise();
   }
 };
--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,24 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+	  return fn.apply(obj);
   },
 
   alterObjects: function(constructor, greeting) {
-
+	  console.log(constructor);
+	  constructor.prototype.greeting = greeting;
   },
 
   iterate: function(obj) {
+	  console.log(obj);
+	  var result = [];
 
+	    for (var key in obj) {
+	      if (obj.hasOwnProperty(key)) {
+	    	  result.push(key + ': ' + obj[key]);
+	      }
+	    }
+	    console.log(result);
+	    return result;
   }
 };
